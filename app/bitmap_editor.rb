39dea9c88c6546c6dc774e3b7a8c372e07f7565f
Robert Lee-Cann <leeky@leeky.org.uk>
2016-04-06 00:04:48 +0100

    Extract out input parsing into its own method
    This allows us to parse any input without having to stub out chomp or
    mess with STDIN/STDOUT.

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -5,27 +5,30 @@ class BitmapEditor
 
     while @running
       print '> '
-      input = gets.chomp
+      parse_input
+    end
+  end
 
-      case input
-      when '?'
-        show_help
-      when 'X'
-        exit_console
-      else
-        puts 'unrecognised command :('
-      end
+  def parse_input(input = gets.chomp)
+    case input
+    when '?'
+      show_help
+    when 'X'
+      exit_console
+    else
+      puts 'unrecognised command :('
     end
   end
 
   private
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
 
-    def show_help
-      puts "? - Help
+  def exit_console
+    puts 'goodbye!'
+    @running = false
+  end
+
+  def show_help
+    puts "? - Help
 I M N - Create a new M x N image with all pixels coloured white (O).
 C - Clears the table, setting all pixels to white (O).
 L X Y C - Colours the pixel (X,Y) with colour C.
@@ -33,5 +36,5 @@ V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 an
 H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 S - Show the contents of the current image
 X - Terminate the session"
-    end
+  end
 end
