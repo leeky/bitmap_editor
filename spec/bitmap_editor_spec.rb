c8e2da3bf23d01cc729f479b71a0174da81de032
Robert Lee-Cann <leeky@leeky.org.uk>
2016-04-06 00:01:03 +0100

    Add simple test which stubs gets.chomp

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe 'BitmapEditor' do
+  describe '#run' do
+    it 'exits when command X is given' do
+      editor = BitmapEditor.new
 
+      stubbed_gets = double
+      expect(editor).to receive(:gets).and_return(stubbed_gets)
+      expect(stubbed_gets).to receive(:chomp).and_return('X')
+
+      expect { editor.run }.to output(/goodbye/).to_stdout
+    end
+  end
 end
